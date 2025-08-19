3d766351d2dac62e7d5d75c76a31ad8c2ecb3caa
RJRYT <robinjratr@gmail.com>
2025-08-19 16:32:37 +0530

    component render order updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,12 +10,12 @@ const Layout = ({ children }) => {
       <main className="container mx-auto px-4 py-8 max-w-7xl">
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
           {/* Main content */}
-          <div className="lg:col-span-3 order-2 lg:order-1 space-y-6">
+          <div className="lg:col-span-3 space-y-6">
             {children}
           </div>
           
           {/* Sidebar */}
-          <div className="lg:col-span-1 order-1 lg:order-2">
+          <div className="lg:col-span-1">
             <Sidebar />
           </div>
         </div>
